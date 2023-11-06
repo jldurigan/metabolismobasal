4224765365120b13192029f4199a73bb75007296
João Lucas <jlucasdurigan@gmail.com>
2023-11-05 23:47:53 -0300

    remoção overlay

--- public/scripts/shared.js
+++ public/scripts/shared.js
@@ -3,26 +3,23 @@ function Inicializar(){
   const customDropdown = document.querySelector('.custom-dropdown');
   const dropdownHeader = document.querySelector('.dropdown-header');
   const lanBarTop = document.querySelector('.lan-bar-top');
-  const overlay = document.querySelector('.overlay');
 
   document.addEventListener("click", function (event) {
     if (dropdownList.style.display === 'block' && !dropdownHeader.contains(event.target)) {
-      toggleDropdown(dropdownList, overlay, lanBarTop, customDropdown);
+      toggleDropdown(dropdownList, lanBarTop, customDropdown);
     }
   });
 
-  dropdownHeader.addEventListener("click", () => toggleDropdown(dropdownList, overlay, lanBarTop, customDropdown));
+  dropdownHeader.addEventListener("click", () => toggleDropdown(dropdownList, lanBarTop, customDropdown));
 }
 
-function toggleDropdown(dropdownList, overlay, lanBarTop, customDropdown) {
+function toggleDropdown(dropdownList, lanBarTop, customDropdown) {
   if (dropdownList.style.display === 'block') {
     dropdownList.style.display = 'none';
-    overlay.style.display = 'none';
     lanBarTop.classList.remove('open');
     customDropdown.classList.remove('open');
   } else {
     dropdownList.style.display = 'block';
-    overlay.style.display = 'block';
     lanBarTop.classList.add('open');
     customDropdown.classList.add('open');
   }
